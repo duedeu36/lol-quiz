ff5c8c3fae1c21a2c3cd36afbe8dcfb545ec094e
duedeu36 <duedeu36@gmail.com>
2019-03-14 13:09:24 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>League of Legends Quiz Game</h1>
+
+        <h3>Main Page</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
